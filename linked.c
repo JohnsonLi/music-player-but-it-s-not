c0a89548e929fa19c1a9031a6ebe9e097732b8d9
Johnson Li <jli39@stuy.edu>
2018-10-15 11:45:33 -0400

    some things work

--- linked.c
+++ linked.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct song_node{ 
   char name[100];
@@ -9,22 +10,40 @@ struct song_node{
 
 void print_list(struct song_node *head){
     while(head){
-        printf("%s: %s |", head->artist, head->name);
+        printf(" %s: %s |", head->artist, head->name);
         head = head->next;
     }
     printf("\n");    
     return;
 }
 
-struct song_node * insert_front(struct song_node *current, char *artist, char *name){
+struct song_node *insert_front(struct song_node *current, char *artist, char *name){
+    struct song_node * to_add = malloc(sizeof(struct song_node));
+    strcpy(to_add->artist, artist);
+    strcpy(to_add->name, name);
+    to_add -> next = current;
+    return to_add;
+}
+
+void insert_order(struct song_node *head, char *artist, char *name){
     struct song_node * to_add = malloc(sizeof(struct song_node));
     to_add -> name = name;
     to_add -> artist = artist;
-    return to_add;
+    
+    while(strcmp(artist, head->artist) <= 0){
+      
+
+    }
 }
 
 int main(){
 
+    struct song_node *head = malloc(sizeof(struct song_node));
+    strcpy(head->artist, "Haydn");
+    strcpy(head->name, "Trumpet Concerto");
+    head = insert_front(head, "Beethoven", "Moonlight Sonata");
+    
+    print_list(head);
 
 
     return 0;
--- makefile
+++ makefile
@@ -1,5 +1,8 @@
-cake:
+cake: linked.o
+	gcc linked.o
 
+linked.o: linked.c
+	gcc -c linked.c
 
 run:
 	./a.out
